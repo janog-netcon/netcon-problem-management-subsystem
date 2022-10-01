cb44ba078e5561afabb3b91619acdc95c7819047
Ryoga Saito <contact@proelbtn.com>
2022-10-01 23:45:57 +0900

    Add auto-generated files: kubebuilder create api Worker

--- main.go
+++ main.go
@@ -30,6 +30,8 @@ import (
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+
+	netconv1alpha1 "github.com/janog-netcon/netcon-problem-management-subsystem/api/v1alpha1"
 	//+kubebuilder:scaffold:imports
 )
 
@@ -41,6 +43,7 @@ var (
 func init() {
 	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
 
+	utilruntime.Must(netconv1alpha1.AddToScheme(scheme))
 	//+kubebuilder:scaffold:scheme
 }
 
