7c3f7ff1c20a83be8f3d45768101c6d17b025bdf
Ryoga Saito <contact@proelbtn.com>
2022-10-04 19:04:06 +0900

    Update fetch_kubeconfig_for_kubelet.sh

--- scripts/fetch_kubeconfig_for_kubelet.sh
+++ scripts/fetch_kubeconfig_for_kubelet.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
-set -ex
+set -e
 
 NAMESPACE="netcon"
-SERVICEACCOUNT_NAME="nclet"
-SECRET_NAME="$(kubectl get -n "${NAMESPACE}" get ServiceAccounts "${SERVICEACCOUNT_NAME}" -o "jsonpath=.status.secrets[0]")"
-TOKEN="$(kubectl get -n "${NAMESPACE}" get Secrets "${SECRET_NAME}" -o "jsonpath=.data.token" | base64 -d)"
+SERVICEACCOUNT_NAME="netcon-nclet"
+SECRET_NAME="$(kubectl get -n "${NAMESPACE}" ServiceAccounts "${SERVICEACCOUNT_NAME}" -o "jsonpath={.secrets[0].name}")"
+TOKEN="$(kubectl get -n "${NAMESPACE}" Secrets "${SECRET_NAME}" -o "jsonpath={.data.token}" | base64 -d)"
 
-echo <<EOF
+cat <<EOF
 kind: Config
 apiVersion: v1
 current-context: netcon
-clusters
+clusters:
 - name: netcon-cplane
   cluster:
     server: {{ server_address }}
