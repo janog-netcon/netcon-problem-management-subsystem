a5b8c153e46557484a91281697cd9877a02ebb1f
Ryoga Saito <contact@proelbtn.com>
2022-11-27 15:41:03 +0900

    Fix default username and password for netcon

--- cmd/nclet/sub.mk
+++ cmd/nclet/sub.mk
@@ -1,10 +1,16 @@
 NCLET_IMG ?= netcon-pms-nclet:dev
 
+DEFAULT_USERNAME ?= username
+DEFAULT_PASSWORD ?= password
+
 ##@ Build: nclet
 
 .PHONY: nclet-docker-build
 nclet-docker-build: test ## Build docker image with the manager.
-	DOCKER_BUILDKIT=1 docker build --file cmd/nclet/Dockerfile -t ${NCLET_IMG} .
+	DOCKER_BUILDKIT=1 docker build --file cmd/nclet/Dockerfile \
+		--build-arg DEFAULT_USERNAME="${DEFAULT_USERNAME}" \
+		--build-arg DEFAULT_PASSWORD="${DEFAULT_PASSWORD}" \
+		-t ${NCLET_IMG} .
 
 .PHONY: nclet-docker-push
 nclet-docker-push: ## Push docker image with the manager.
