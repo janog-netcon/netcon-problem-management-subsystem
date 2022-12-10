8a7aaa40d6ed1b49d3868709a6f13f5d29b4d8b7
motacapla <t.i.motacapla@gmail.com>
2022-12-10 17:47:52 +0900

    initial impl with echo

--- controllers/gateway/gateway.go
+++ controllers/gateway/gateway.go
@@ -2,11 +2,15 @@ package controllers
 
 import (
 	"context"
+	"net/http"
 
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 	"sigs.k8s.io/controller-runtime/pkg/runtime/inject"
+
+	echo "github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 type Gateway struct {
@@ -25,6 +29,21 @@ func (g *Gateway) Start(ctx context.Context) error {
 	_ = log.FromContext(ctx)
 
 	// TODO: spawn HTTP server
+	// Echo instance
+	e := echo.New()
+
+	// Middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	e.GET("/health", health)
+
+	e.Logger.Fatal(e.Start(":8080"))
 
 	return nil
+}
+
+// ハンドラーを定義
+func health(c echo.Context) error {
+	return c.String(http.StatusOK, "health")
 }
