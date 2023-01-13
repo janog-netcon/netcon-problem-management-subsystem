f51deb4cc309b04656330779fc15a543a5334c6b
Ryoga Saito <contact@proelbtn.com>
2023-01-13 17:44:04 +0900

    Update sub.mk

--- cmd/kubectl-netcon/sub.mk
+++ cmd/kubectl-netcon/sub.mk
@@ -1,5 +1,5 @@
 ##@ Build: kubectl-netcon
 
-.PHONY: kubectl-netcon-docker-build
-kubectl-netcon-docker-build: ## Build kubectl-netcon
+.PHONY: kubectl-netcon-build
+kubectl-netcon-build: ## Build kubectl-netcon
 	go build -o kubectl-netcon ./cmd/kubectl-netcon
