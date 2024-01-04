2af1641a322e29f6a12fbf00fb43a36b00732fb7
Ryoga Saito <ryoga.saito@lycorp.co.jp>
2024-01-04 04:56:54 +0000

    変数名を修正した

--- cmd/nclet/sub.mk
+++ cmd/nclet/sub.mk
@@ -1,15 +1,15 @@
 NCLET_IMG ?= netcon-pms-nclet:dev
 
-DEFAULT_USERNAME ?= username
-DEFAULT_PASSWORD ?= password
+DEFAULT_SSH_USERNAME ?= username
+DEFAULT_SSH_PASSWORD ?= password
 
 ##@ Build: nclet
 
 .PHONY: nclet-docker-build
 nclet-docker-build: test ## Build docker image with the manager.
 	DOCKER_BUILDKIT=1 docker build --file cmd/nclet/Dockerfile \
-		--build-arg DEFAULT_USERNAME="${DEFAULT_USERNAME}" \
-		--build-arg DEFAULT_PASSWORD="${DEFAULT_PASSWORD}" \
+		--build-arg DEFAULT_SSH_USERNAME="${DEFAULT_SSH_USERNAME}" \
+		--build-arg DEFAULT_SSH_PASSWORD="${DEFAULT_SSH_PASSWORD}" \
 		-t ${NCLET_IMG} .
 
 .PHONY: nclet-docker-push
