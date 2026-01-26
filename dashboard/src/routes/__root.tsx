0f9265c5e44b0be193452acede7afe454afbfa0e
Ryoga Saito <contact@proelbtn.com>
2026-01-26 23:31:38 +0900

    Remove unnecessary files and set favicon

--- dashboard/src/routes/__root.tsx
+++ dashboard/src/routes/__root.tsx
@@ -25,6 +25,11 @@ export const Route = createRootRoute({
         rel: 'stylesheet',
         href: appCss,
       },
+      {
+        rel: 'icon',
+        href: '/favicon.svg',
+        type: 'image/svg+xml',
+      }
     ],
   }),
 
