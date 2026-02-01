795137e94cef24d2c981f3b0eefc7b892a2df663
Ryoga Saito <contact@proelbtn.com>
2026-02-01 13:28:18 +0900

    Fix SSH-related issues

--- controllers/nclet/ssh_server.go
+++ controllers/nclet/ssh_server.go
@@ -224,7 +224,7 @@ func (r *SSHServer) handle(ctx context.Context, s ssh.Session) error {
 		args = append(args, user.NodeName)
 	}
 
-	cmd := exec.Command("access-helper", args...)
+	cmd := exec.CommandContext(ctx, "access-helper", args...)
 
 	if otlpEndpoint != "" {
 		cmd.Env = append(syscall.Environ(), fmt.Sprintf("%s=%s", tracing.OTLPEndpointKey, otlpEndpoint))
@@ -238,13 +238,14 @@ func (r *SSHServer) handle(ctx context.Context, s ssh.Session) error {
 	if err != nil {
 		return tracing.WrapError(span, err, "failed to start pty")
 	}
+	defer ptmx.Close()
 
 	// TODO: handle terminal resize
 
 	go func() { _, _ = io.Copy(ptmx, s) }()
-	_, _ = io.Copy(s, ptmx)
+	go func() { _, _ = io.Copy(s, ptmx) }()
 
-	s.Close()
+	cmd.Wait()
 
 	return nil
 }
@@ -253,7 +254,8 @@ func (r *SSHServer) Start(ctx context.Context) error {
 	_ = log.FromContext(ctx)
 
 	server := &ssh.Server{
-		Addr: r.sshAddr,
+		Addr:        r.sshAddr,
+		IdleTimeout: 20 * time.Minute,
 		PasswordHandler: func(sctx ssh.Context, password string) bool {
 			ctx, span := tracing.Tracer.Start(sctx, "Server#PasswordHandler")
 			defer span.End()
@@ -277,6 +279,8 @@ func (r *SSHServer) Start(ctx context.Context) error {
 			return true
 		},
 		Handler: func(s ssh.Session) {
+			defer s.Close()
+
 			sshSessionsInFlight.Inc()
 			defer sshSessionsInFlight.Dec()
 
